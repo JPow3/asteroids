3db22ab061119dfc159ce91c3a623a365b7ed143
Joe Pow <joepow@ultra.me>
2025-05-23 10:39:21 -0400

    added player.py and updated main.py

--- main.py
+++ main.py
@@ -2,18 +2,25 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-import constants
+import constants as c
+import player as p
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = p.Player((c.SCREEN_WIDTH/2), (c.SCREEN_HEIGHT/2))
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, color="black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
